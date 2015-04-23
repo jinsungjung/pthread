d2bc5cf5c7f22484e2e0b1b3462cd6e59c79ac23
Jinsung Jung <zeromil1@naver.com>
2015-04-23 16:50:42 +0900

    new thread2

--- pthread_test1.c
+++ pthread_test1.c
@@ -14,7 +14,7 @@ void* Thread( void *arg )
 {
     int i;
     for ( i=0; i<30; i++ )
-        printf( "new my thread %d: %dth iteration!!!\n", *(int*)arg, i );
+        printf( "new22 my thread %d: %dth iteration!!!\n", *(int*)arg, i );
     pthread_exit(0);  /* 쓰레드 종료 함수 */
 }
 //-------------------------------------------------------------
